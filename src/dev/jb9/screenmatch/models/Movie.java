d8d7480b8b961b1def003e04a787c245a87c8f56
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-09-25 23:30:52 +0100

    feat: implements Movie as Sortable

--- src/dev/jb9/screenmatch/models/Movie.java
+++ src/dev/jb9/screenmatch/models/Movie.java
@@ -1,8 +1,13 @@
 package dev.jb9.screenmatch.models;
 
-public class Movie extends Asset {
-    private String director;
+import dev.jb9.screenmatch.calculations.Sortable;
 
+public class Movie extends Asset implements Sortable {
+    // region fields
+    private String director;
+    // endregion fields
+
+    // region getters and setters
     public String getDirector() {
         return director;
     }
@@ -10,4 +15,12 @@ public class Movie extends Asset {
     public void setDirector(String director) {
         this.director = director;
     }
+    // endregion getters and setters
+
+    // region implementations
+    @Override
+    public int getSorting() {
+        return (int) getAverage() / 2;
+    }
+    // endregion implementations
 }
