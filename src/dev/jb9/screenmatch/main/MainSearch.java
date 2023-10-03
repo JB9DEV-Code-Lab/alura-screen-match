0f19d227d3c83b3531c1a14489fa8662745c0e1e
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-10-03 23:16:43 +0100

    feat: make use of Gson to build json

--- src/dev/jb9/screenmatch/main/MainSearch.java
+++ src/dev/jb9/screenmatch/main/MainSearch.java
@@ -1,5 +1,11 @@
 package dev.jb9.screenmatch.main;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dev.jb9.screenmatch.dtos.AssetDTO;
+import dev.jb9.screenmatch.models.Asset;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -17,6 +23,11 @@ public class MainSearch {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endpoint)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+        AssetDTO assetDTO = gson.fromJson(response.body(), AssetDTO.class);
+        System.out.println("AssetDTO: " + assetDTO);
+        Asset assetFromSearchTerm = new Asset(assetDTO);
+        System.out.println("Built asset from search term: " + assetFromSearchTerm);
     }
 }
