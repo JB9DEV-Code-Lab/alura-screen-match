6936c9638ab8e078bd0a9c737bce0dec8d9e5901
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-10-04 23:11:00 +0100

    feat: loop asking for many movies to search

--- src/dev/jb9/screenmatch/main/MainSearch.java
+++ src/dev/jb9/screenmatch/main/MainSearch.java
@@ -4,24 +4,42 @@ import dev.jb9.screenmatch.dtos.OMDBAssetDTO;
 import dev.jb9.screenmatch.models.Asset;
 import dev.jb9.screenmatch.services.OMDBService;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MainSearch {
     public static void main(String[] args)  {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Which movie do you want to search? \n> ");
-        String movieName = reader.nextLine();
+        OMDBService omdbService = new OMDBService();
+        File searchedMoviesJson = new File("src/dev/jb9/screenmatch/resources/searched-movies.json");
+
+        System.out.println("""
+        Hi, this is the movie searcher. I'm keep asking you which movie you want to search until you say "done".
+        Then I'll store all of them in a file at the end and I'll let you know.
+        """);
+        String movieName = "";
+
+        while(true) {
+            try {
+                System.out.print("Which movie do you want to search? \n> ");
+                movieName = reader.nextLine().toLowerCase();
+
+                if (movieName.equals("done")) {
+                    break;
+                }
+
+                OMDBAssetDTO searchedMovie = omdbService.searchMovie(movieName);
+                omdbService.saveSearchedMovie(searchedMovie);
+            } catch (IOException | InterruptedException exception) {
+                System.out.println("Couldn't search for movie. " + exception.getMessage());
+            }
+        }
 
         try {
-            OMDBAssetDTO omdbAssetDTO = OMDBService.searchMovie(movieName);
-            Asset assetFromSearchTerm = new Asset(omdbAssetDTO);
-            System.out.println("OMDBAssetDTO: " + omdbAssetDTO);
-            System.out.println("Built asset from search term: " + assetFromSearchTerm);
-        } catch (IOException | InterruptedException exception) {
-            System.out.println("Couldn't search for movie. " + exception.getMessage());
-        } finally {
-            System.out.println("End of program!");
+            omdbService.saveMovies(searchedMoviesJson);
+        } catch (IOException exception) {
+            System.out.println("Couldn't save searched movie: " + exception.getMessage());
         }
     }
 }
