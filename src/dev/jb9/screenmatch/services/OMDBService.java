8313a921f7cd0b0f06afe1c5045877661f8de7eb
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-10-04 23:10:32 +0100

    feat: save searched movies to a file

--- src/dev/jb9/screenmatch/services/OMDBService.java
+++ src/dev/jb9/screenmatch/services/OMDBService.java
@@ -1,9 +1,13 @@
 package dev.jb9.screenmatch.services;
 
 import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dev.jb9.screenmatch.dtos.OMDBAssetDTO;
+import dev.jb9.screenmatch.models.Asset;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -11,10 +15,17 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OMDBService {
+    private final List<Asset> searchedMovies = new ArrayList<>();
+    private final Gson gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+            .setPrettyPrinting()
+            .create();
 
-    public static OMDBAssetDTO searchMovie(String term) throws IOException, InterruptedException {
+    public OMDBAssetDTO searchMovie(String term) throws IOException, InterruptedException {
         String searchTerm = URLEncoder.encode(term, StandardCharsets.UTF_8);
         String endpoint = "https://www.omdbapi.com?t=" + searchTerm + "&apikey=" + System.getenv("OMDB_API_KEY");
 
@@ -22,9 +33,26 @@ public class OMDBService {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endpoint)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        return new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create()
-                .fromJson(response.body(), OMDBAssetDTO.class);
+        if (response.statusCode() >= 200 && response.statusCode() < 300) {
+            System.out.printf("%nMovie %s successfuly found!%n%n", term);
+        } else {
+            System.out.printf("%nCouldn't find movie %s%n%n", term);
+        }
+
+        return gson.fromJson(response.body(), OMDBAssetDTO.class);
+    }
+
+    public void saveSearchedMovie(OMDBAssetDTO searchedMovie) {
+        Asset asset = new Asset(searchedMovie);
+        searchedMovies.add(asset);
+    }
+
+    public void saveMovies(File file) throws IOException {
+        String json = gson.toJson(searchedMovies);
+        FileWriter fileWriter = new FileWriter(file);
+
+        fileWriter.write(json);
+        fileWriter.close();
+        System.out.println("Searched movies saved at " + file.getPath());
     }
 }
